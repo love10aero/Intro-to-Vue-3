fe52d6da61d81f103a18d99349d2e697dafcb6d3
Love <love.tr2014@gmail.com>
2024-05-22 20:14:28 +0200

    Lesson 3 finished

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'My Portfolio',
+            image: './assets/images/squarepict.jpeg',
+            url: 'https://love-singh.vercel.app/'
         }
     }
 })
