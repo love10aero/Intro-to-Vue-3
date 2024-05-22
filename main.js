28efc774d6dcfb3e720afe685806c70637e696a6
Love <love.tr2014@gmail.com>
2024-05-22 22:43:28 +0200

    Lesson 8 finished

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ const app = Vue.createApp({
     data() {
         return {
             cart: 0,
-            image: './assets/images/socks_blue.jpg',
+            brand: 'Love',
             product: 'Socks',
-            inStock: false,
+            selectedVariant: 0,
             activeClass: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 1, color: 'blue', image: './assets/images/socks_blue.jpg'},
-                {id: 2, color: 'green', image: './assets/images/socks_green.jpg',}
+                {id: 1, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 50, onSale: false},
+                {id: 2, color: 'green', image: './assets/images/socks_green.jpg', quantity: 0, onSale: true}
             ],
             sizes: ['S', 'M', 'L']
         }
@@ -18,11 +18,28 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1;
         },
-        changeImage(imageVariant) {
-            this.image = imageVariant;
+        changeVariant(index) {
+            this.selectedVariant = index;
         },
         removeOneFromCart() {
             this.cart -= 1
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity > 0
+        },
+        onSale() {
+            if (this.variants[this.selectedVariant].onSale) {
+                return this.brand + ' ' + this.product + ' is on sale!'
+            }
+            return 
         }
     }
 })
