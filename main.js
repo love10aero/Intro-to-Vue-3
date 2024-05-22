2504fd6d600e1f0fe7104f02f6ef75616ac93e8a
Love <love.tr2014@gmail.com>
2024-05-22 21:02:25 +0200

    Lesson 5 finished

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks',
+            cart: 0,
             image: './assets/images/socks_blue.jpg',
+            product: 'Socks',
             inStock: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id: 1, color: 'blue'},
-                {id: 2, color: 'red'}
+                {id: 1, color: 'blue', image: './assets/images/socks_blue.jpg'},
+                {id: 2, color: 'green', image: './assets/images/socks_green.jpg',}
             ],
             sizes: ['S', 'M', 'L']
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        changeImage(imageVariant) {
+            this.image = imageVariant;
+        },
+        removeOneFromCart() {
+            this.cart -= 1
         }
     }
 })
